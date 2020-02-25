fc7b8e1566140325fdc2bc00ef71ad59a6ea2616
Alan Li <alandron06281990@gmail.com>
2020-02-24 21:27:00 -0600

    add createManager

--- expenseReimbursementSystem/src/main/java/com/revature/project1/driver/Driver.java
+++ expenseReimbursementSystem/src/main/java/com/revature/project1/driver/Driver.java
@@ -3,9 +3,16 @@ package com.revature.project1.driver;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.mindrot.jbcrypt.BCrypt;
+
+import com.revature.project1.daos.EmployeeDao;
+import com.revature.project1.daos.EmployeeDaoImpl;
+import com.revature.project1.models.Employee;
 import com.revature.project1.util.ConnectionUtil;
 
 public class Driver {
+	
+	private static EmployeeDao ed = new EmployeeDaoImpl();
 
 	public static void main(String[] args) {
 		
@@ -16,6 +23,25 @@ public class Driver {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
+		createManager();
+	}
+	
+	public static void createManager() {
+		String email = "alan@l.com";
+		String type = "manager";
+		String first_name = "alan";
+		String last_name = "li";
+		String hashedPw = BCrypt.hashpw("password", BCrypt.gensalt());
+		
+		Employee manager = new Employee();
+		manager.setEmail(email);
+		manager.setEmployee_type(type);
+		manager.setFirst_name(first_name);
+		manager.setLast_name(last_name);
+		manager.setPass(hashedPw);
+		
+		ed.createEmployee(manager);
 	}
 
 }
---
+++ expenseReimbursementSystem/src/main/java/com/revature/project1/services/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.revature.project1.services;
+
+import com.revature.project1.daos.EmployeeDao;
+import com.revature.project1.daos.EmployeeDaoImpl;
+import com.revature.project1.models.Employee;
+
+public class EmployeeService {
+	
+	private EmployeeDao ed = new EmployeeDaoImpl();
+
+	public int createManager(Employee e) {
+		return ed.createEmployee(e);
+	}
+}
