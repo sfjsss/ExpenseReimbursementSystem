e00d61d059d90fff5dbc085a43197d55d41d7ad5
Tianyu Li <alandron06281990@gmail.com>
2020-02-27 13:59:40 -0600

    done

---
+++ expenseReimbursementSystem/src/main/java/com/revature/project1/controller/FrontController.java
@@ -0,0 +1,32 @@
+package com.revature.project1.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public FrontController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String path = request.getServletPath();
+		System.out.println(path);
+		
+		if (path.startsWith("/static")) {
+			super.doGet(request, response);
+		} else {
+			
+		}
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
