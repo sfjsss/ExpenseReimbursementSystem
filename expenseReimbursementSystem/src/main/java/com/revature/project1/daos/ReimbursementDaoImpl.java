795cacc1e46d106f705d5498596a8ce52ca36541
Tianyu Li <alandron06281990@gmail.com>
2020-02-27 23:13:46 -0600

    done

--- expenseReimbursementSystem/src/main/java/com/revature/project1/daos/ReimbursementDaoImpl.java
+++ expenseReimbursementSystem/src/main/java/com/revature/project1/daos/ReimbursementDaoImpl.java
@@ -33,27 +33,28 @@ public class ReimbursementDaoImpl implements ReimbursementDao {
 				rb.setReimbursement_id(rs.getInt(1));
 				rb.setReimbursement_type(rs.getString(2));
 				rb.setReimbursement_time(rs.getTimestamp(3));
-				rb.setReimbursement_description(rs.getString(4));
-				rb.setReceipt_name(rs.getString(5));
-				rb.setReceipt_path(rs.getString(6));
-				rb.setReimbursement_status(rs.getString(7));
+				rb.setReimbursement_amount(rs.getDouble(4));
+				rb.setReimbursement_description(rs.getString(5));
+				rb.setReceipt_name(rs.getString(6));
+				rb.setReceipt_path(rs.getString(7));
+				rb.setReimbursement_status(rs.getString(8));
 				
 				Employee requester = new Employee();
-				requester.setEmployee_id(rs.getInt(10));
-				requester.setEmail(rs.getString(11));
-				requester.setEmployee_type(rs.getString(12));
-				requester.setFirst_name(rs.getString(13));
-				requester.setLast_name(rs.getString(14));
-				requester.setPass(rs.getString(15));
+				requester.setEmployee_id(rs.getInt(11));
+				requester.setEmail(rs.getString(12));
+				requester.setEmployee_type(rs.getString(13));
+				requester.setFirst_name(rs.getString(14));
+				requester.setLast_name(rs.getString(15));
+				requester.setPass(rs.getString(16));
 				rb.setRequester(requester);
 				
 				Employee processor = new Employee();
-				processor.setEmployee_id(rs.getInt(16));
-				processor.setEmail(rs.getString(17));
-				processor.setEmployee_type(rs.getString(18));
-				processor.setFirst_name(rs.getString(19));
-				processor.setLast_name(rs.getString(20));
-				processor.setPass(rs.getString(21));
+				processor.setEmployee_id(rs.getInt(17));
+				processor.setEmail(rs.getString(18));
+				processor.setEmployee_type(rs.getString(19));
+				processor.setFirst_name(rs.getString(20));
+				processor.setLast_name(rs.getString(21));
+				processor.setPass(rs.getString(22));
 				rb.setProcessor(processor);
 				
 				reimbursements.add(rb);
@@ -76,7 +77,7 @@ public class ReimbursementDaoImpl implements ReimbursementDao {
 
 	@Override
 	public int createNewReimbursement(Reimbursement r) {
-		String sql = "insert into reimbursement (reimbursement_type, reimbursement_time, reimbursement_description, receipt_name, receipt_path, reimbursement_status, requester_id, processor_id) values (?, ?, ?, ?, ?, ?, ?, ?)";
+		String sql = "insert into reimbursement (reimbursement_type, reimbursement_time, reimbursement_amount, reimbursement_description, receipt_name, receipt_path, reimbursement_status, requester_id, processor_id) values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		int affectedRows = 0;
 		
 		try (Connection c = ConnectionUtil.getConnection();
@@ -84,12 +85,13 @@ public class ReimbursementDaoImpl implements ReimbursementDao {
 			
 			ps.setString(1, r.getReimbursement_type());
 			ps.setTimestamp(2, r.getReimbursement_time());
-			ps.setString(3, r.getReimbursement_description());
-			ps.setString(4, r.getReceipt_name());
-			ps.setString(5, r.getReceipt_path());
-			ps.setString(6, r.getReimbursement_status());
-			ps.setInt(7, r.getRequester().getEmployee_id());
-			ps.setInt(8, r.getProcessor().getEmployee_id());
+			ps.setDouble(3, r.getReimbursement_amount());
+			ps.setString(4, r.getReimbursement_description());
+			ps.setString(5, r.getReceipt_name());
+			ps.setString(6, r.getReceipt_path());
+			ps.setString(7, r.getReimbursement_status());
+			ps.setInt(8, r.getRequester().getEmployee_id());
+			ps.setInt(9, r.getProcessor().getEmployee_id());
 			
 			affectedRows = ps.executeUpdate();
 			
@@ -118,27 +120,28 @@ public class ReimbursementDaoImpl implements ReimbursementDao {
 				rb.setReimbursement_id(rs.getInt(1));
 				rb.setReimbursement_type(rs.getString(2));
 				rb.setReimbursement_time(rs.getTimestamp(3));
-				rb.setReimbursement_description(rs.getString(4));
-				rb.setReceipt_name(rs.getString(5));
-				rb.setReceipt_path(rs.getString(6));
-				rb.setReimbursement_status(rs.getString(7));
+				rb.setReimbursement_amount(rs.getDouble(4));
+				rb.setReimbursement_description(rs.getString(5));
+				rb.setReceipt_name(rs.getString(6));
+				rb.setReceipt_path(rs.getString(7));
+				rb.setReimbursement_status(rs.getString(8));
 				
 				Employee requester = new Employee();
-				requester.setEmployee_id(rs.getInt(10));
-				requester.setEmail(rs.getString(11));
-				requester.setEmployee_type(rs.getString(12));
-				requester.setFirst_name(rs.getString(13));
-				requester.setLast_name(rs.getString(14));
-				requester.setPass(rs.getString(15));
+				requester.setEmployee_id(rs.getInt(11));
+				requester.setEmail(rs.getString(12));
+				requester.setEmployee_type(rs.getString(13));
+				requester.setFirst_name(rs.getString(14));
+				requester.setLast_name(rs.getString(15));
+				requester.setPass(rs.getString(16));
 				rb.setRequester(requester);
 				
 				Employee processor = new Employee();
-				processor.setEmployee_id(rs.getInt(16));
-				processor.setEmail(rs.getString(17));
-				processor.setEmployee_type(rs.getString(18));
-				processor.setFirst_name(rs.getString(19));
-				processor.setLast_name(rs.getString(20));
-				processor.setPass(rs.getString(21));
+				processor.setEmployee_id(rs.getInt(17));
+				processor.setEmail(rs.getString(18));
+				processor.setEmployee_type(rs.getString(19));
+				processor.setFirst_name(rs.getString(20));
+				processor.setLast_name(rs.getString(21));
+				processor.setPass(rs.getString(22));
 				rb.setProcessor(processor);
 				
 				reimbursements.add(rb);
